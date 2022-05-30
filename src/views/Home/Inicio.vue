8a1c8cf969d1fc9b223fafcbd57577964ffa1317
Pbluer <ramonpvlima2@hotmail.com>
2022-05-30 16:57:22 -0300

    0.0.1.0

--- src/views/Home/Inicio.vue
+++ src/views/Home/Inicio.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-[50rem] bg-white flex flex-col space-y-10 justify-center items-center
+  <div class="w-full h-[50rem] bg-white dark:bg-gray-700 flex flex-col space-y-10 justify-center items-center
   md:h-[40rem]
   md:flex-row
   lg:space-x-[7rem]
@@ -27,7 +27,7 @@
     <h1 class="text-3xl font-bold text-[#FFC727]
     md:text-4xl
     ">Ramon Vasconcelos</h1>
-    <h3 class="text-2xl font-bold text-gray-700">Desenvolvedor Fullstack</h3>
+    <h3 class="text-2xl font-bold dark:text-white text-gray-700">Desenvolvedor Fullstack</h3>
   </div>
 
   </div>
