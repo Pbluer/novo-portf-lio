cd85602162d0c1a51d55ba483c9819fb6cbfbbb4
Pluer <ramonpvlima2@hotmail.com>
2024-03-22 22:08:34 -0300

    retirar projeto

--- src/views/Home/Projetos.vue
+++ src/views/Home/Projetos.vue
@@ -26,14 +26,6 @@
                     <p class="text-[#3F3F46]">Aplicação desenvolvida como estudo usando as tecnolgias do framework Vue.</p>
                 </div>               
             </button>
-            
-            <button @click="goinTo('https://todovuephp.000webhostapp.com/')" class="w-[22rem] h-28 p-3 flex justify-center items-center flex-row bg-white rounded-2xl shadow-lg">               
-                <div class="w-[20rem] text-left space-y-2">
-                    <h3 class="font-bold text-[#3F3F46] flex flex-row space-x-2 "><span>Todo-list</span>  <img src="@/assets/icon/link.svg" alt="link" class="w-4"> </h3>
-                    <p class="text-[#3F3F46]">Aplicação desenvolvida como estudo usando as tecnolgias Vue 3, Tailwind e MySQL.</p>
-                </div>                   
-            </button>
-
         </div>
 
     </div>
